9b4f6256da64ba7727038fda86444fc70de31d3f
Tomi Topaasia <tomitopaasia@gmail.com>
2025-09-22 15:29:13 +0300

    newcommit

---
+++ index.js
@@ -0,0 +1,35 @@
+let num = 1;
+console.log(num);
+console.log(typeof num);
+num = 1.2;
+console.log(num);
+console.log(typeof num);
+let text = "hello world";
+console.log(text);
+console.log(typeof text);
+text = "H"; //char
+console.log(text);
+console.log(typeof text);
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+let undef= undefined;
+console.log(undef);
+console.log(typeof undef);
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+let array = [1, 2, "Hello", "World", true [1, 2, 3]];
+console.log(array);
+console.log(typeof array);
+let obj = {
+    name: "Tomi",
+    age: 20,
+    hairColor: "Brunette",
+    isMale: true,
+};
+console.log(obj);
+console.log(typeof obj);
